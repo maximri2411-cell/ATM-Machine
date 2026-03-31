f59877ddecf7e5a420b87fdac0f39d4f4c823861
Alex.dv <alexdv@gmail.com>
2026-03-31 23:01:54 +0300

    Start main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from models import Accounts
+
 print("Welcome to the Passover Project!")
+account = int(input("please enter your id:"))
+for acc in all_accounts: 
+    if acc.account_id == user_id:
+        acc.get_info()
+    else:
+        print("You are out of the system")
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 #!Pay attention its only for class Account & Bank
 
 class Accounts:
-    
+  
     def __init__(self, account_id, name, PIN, Balance, is_active , History):
         self.account_id = account_id
         self.name = name
@@ -23,16 +23,14 @@ class Accounts:
             print("Can't continue the process, plaese call customer service")
 
 
-
-# 
-
-
 account1 = Accounts(321321, "Grisha", 1234, 50000, True, [])
 # account2 = Accounts(653421, "Daniel", 4332, 20000, "True", [])
 account3 = Accounts(132456, "Moshe", 4343, 15000, "False", [])
 # account4 = Accounts(314265, "Michal", 2323, 37000, "False", [])
 # account5 = Accounts(556612, "Dana", 1123, 43000, "True", [])
 # account6 = Accounts(615243, "Shahar", 2121, 29000, "False", [])
+
+all_accounts = [account1, account3]
 
 account1.get_info()
 # account2.get_info()
