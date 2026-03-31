69179df641419a82923d727c03b02273735e2067
Maxim <maximri2411@gmail.com>
2026-03-31 19:05:18 +0300

    Test

--- models.py
+++ models.py
@@ -1,2 +1,28 @@
-#!Pay attention its only for class Account & Bank
+#=============== Alex ===============
 
+
+
+
+#=============== Alex ===============
+
+
+
+
+
+
+#=============== Maxim ===============
+class Bank:
+    def
+
+
+    def create_new_account(self, pin):
+        
+    def find_account(self, name, ):
+     
+    def login_account(self, pin, ):
+    
+    def manager_login(self, pin, ):
+        
+    def transfer_money(self, ):
+        
+    def all_accounts(self):
