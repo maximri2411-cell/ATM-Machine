18ecfdae09f89d08d1a1d4aa1aade0c619ea0de1
Alex.dv <alexdv@gmail.com>
2026-03-31 19:00:24 +0300

    Account structure building

--- models.py
+++ models.py
@@ -1,2 +1,20 @@
 #!Pay attention its only for class Account & Bank
 
+class Accounts:
+    
+    def __init__(self, account_id, name, PIN, Balance, Status, History):
+        self.account_id = id
+        self.name = name
+        self.PIN = PIN
+        self.Balance = Balance
+        self.Status = Status
+        self.History = History
+        
+        
+    def get_info(self):
+        return(self.account_id, self.name, self.PIN, self.Balance, self.Status,  self.History)
+        
+
+account1 = Accounts(12345, "grisha", 1234, 50000, "Status", "History")
+
+account1.get_info
