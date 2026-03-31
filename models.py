516be83814d967122d868a6bb216d66b92b3a959
Maxim <maximri2411@gmail.com>
2026-04-01 00:47:01 +0300

    Good night

--- models.py
+++ models.py
@@ -16,8 +16,7 @@ class Accounts:
         
 
 
-# account1 = Accounts(12345, "Grisha", 1234, 50000, "Activate", "History")
-# =======
+
 # account1 = Accounts(321321, "Grisha", 1234, 50000, "Status", "History")
 # account2 = Accounts(653421, "Daniel", 4332, 20000, "Status", "History")
 # account3 = Accounts(132456, "Moshe", 4343, 15000, "Status", "History")
@@ -31,47 +30,82 @@ class Accounts:
 
 
 #=======================
-# Part 3: Bank manager 
+# Part 3: Bank manager #TODO find a way to make it a dic
 #=======================
 import random 
 
 
-#Creating a class of the Bank Part 3: it will manager all the accounts in the bank
+# Creating a class of the Bank Part 3: it will manager all the accounts in the bank
 class Bank: 
     
     def __init__(self):
-        self.Manager_pin = "admin123456123456" #managers password
-        self.Accounts = {} #some kind of dictionary to save all of the accounts
+        
+        self.Manager_name = "King-Kong"
+        self.Manager_pin = "admin123456123456" # Managers password
+        self.Accounts = {} # Some kind of dictionary to save all of the accounts
     
     
-    #Function to create new account 
+    # Function to create new account 
     def create_account(self, name, pin):
-        account_ID = str(random.randint(100000, 999999)) #!Alex created users ID using 6 digits
+        account_id = str(random.randint(100000, 999999)) #!Alex created users ID using 6 digits
     
-        while account_ID in self.Accounts:
-            account_ID = str(random.randint(100000, 999999))
+        while account_id in self.Accounts:
+            account_id = str(random.randint(100000, 999999))
         
-        new_account = Accounts(account_ID, name, pin, 0.0, "Active", []) #creating a user exactly according to the characteristics we created in part 2
+        new_account = Accounts(account_id, name, pin, 0.0, "Active", []) # Creating a user exactly according to the characteristics we created in part 2
         
-        self.Accounts[account_ID] = new_account #saving the new account in the dictionary of the bank
+        self.Accounts[account_id] = new_account # saving the new account in the dictionary of the bank
         
-        print(f"Account has been created! Your ID is: {account_ID}") #input of the creation
-        return account_ID
+        print(f"Account has been created. Your ID is: {account_id}") # input of the creation
+        return account_id
 
-    # #Function to find an account
-    # def find_account(self, ):
+    # Function to find an account
+    def find_account(self, account_id):
         
-    # #  Function to log a user into the bank
-    # def login_account(self, ):
+        account_search = self.Accounts.get(int(account_id)) # The get() is to print None in case the ID hasent been found
         
-    # #Function of the manager
-    # def manager_login(self, ):
+        if account_search:
+            print(f"User found: {account_search.name}")
+            return account_search
+        else:
+            print("Error: Account ID was not found.")
+            return None
+        
+    #  Function to log a user into the bank
+    def login_account(self, account_id, pin):
+        
+        account = self.find_account(account_id) # Uses the function we created in previos function
+        
+        if account:
+            if account.pin == int(pin):
+                print((f"Login successful. \nWelcome {account.name}."))
+                return account
+            else:
+                print("Error: Incorerect PIN. \nPlease try again. \nIn case you forgot the PIN, Conntact your local Bank.")
+                return None
+            
+        return None 
+        
+    #Function of the manager
+    def manager_login(self, password):
+        if password == self.Manager_pin:
+            print(f"Manager access granted. \nWelcome {self.Manager_name}") # We determined in the beginning inside the "Father" 
+            return True
+        else:
+            print("Access Denied: Invalid manager password. \nPlease try again. \nIn case you forgot, Contact your management.")
     
     # #Function for transfer money between accounts
     # def transfer(self, ):
         
-    # #Function of list all the accounts we created
-    # def list_accounts(self, )ֿ:
+    #Function of list all the accounts we created
+    def list_accounts(self):
+        print("----- Bank Accounts List -----") # Nice title
+        
+        for account in self.Accounts.values():
+            
+            print(f"ID: {account.account_id} | Name: {account.name} | Balance: {account.Balance}")
+        
+        print("-------------------------------")
     
     
     
@@ -81,17 +115,37 @@ class Bank:
     
     
     
-# #!Check to see if the create account works
-# my_bank = Bank()
+#!Check to see if the create account works
+my_bank = Bank()
 
-# print("--- Testing Account Creation ---")
-# new_id = my_bank.create_account("Yossi Levi", "1234")
+print("")
+print("--- Testing Account Creation ---")
+print("")
+new_id = my_bank.create_account("Yossi Levi", "1234")
 
-# if new_id in my_bank.Accounts:
-#     print(f"Success! Account {new_id} exists in the bank.")
+if new_id in my_bank.Accounts:
+    print(f"Success! Account {new_id} exists in the bank.")
     
-#     user_account = my_bank.Accounts[new_id]
-#     print(f"Owner Name: {user_account.name}")
-#     print(f"Current Balance: {user_account.Balance}")
-# else:
-#     print("Failure: Account was not saved in the dictionary.")
+    user_account = my_bank.Accounts[new_id]
+    print(f"Owner Name: {user_account.name}")
+    print(f"Current Balance: {user_account.Balance}")
+else:
+    print("Failure: Account was not saved in the dictionary.")
+    
+print("")
+    
+
+#! Check if account has beem found
+my_bank = Bank()
+
+new_id = my_bank.create_account("Grisha", "1234")
+
+search_result = my_bank.find_account(new_id)
+
+if search_result:
+    print(f"The balance for {search_result.name} is: {search_result.Balance}")
+    
+print("")
+
+my_bank.login_account("123456", "0000") # אמור להצליח
+my_bank.login_account("123456", "9999") # אמור להיכשל (PIN טועה)
