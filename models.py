d488a21f8923b32978ea476f8114cd9d12ad5656
Alex.dv <alexdv@gmail.com>
2026-03-31 20:43:29 +0300

    Create 6 accounts

--- models.py
+++ models.py
@@ -15,6 +15,11 @@ class Accounts:
         print(self.account_id, self.name, self.PIN, self.Balance, self.Status,  self.History)
         
 
-account1 = Accounts(12345, "grisha", 1234, 50000, "Status", "History")
+account1 = Accounts(321321, "Grisha", 1234, 50000, "Status", "History")
+account2 = Accounts(653421, "Daniel", 4332, 20000, "Status", "History")
+account3 = Accounts(132456, "Moshe", 4343, 15000, "Status", "History")
+account4 = Accounts(314265, "Michal", 2323, 37000, "Status", "History")
+account5 = Accounts(556612, "Dana", 1123, 43000, "Status", "History")
+account6 = Accounts(615243, "Shahar", 2121, 29000, "Status", "History")
 
 account1.get_info()
