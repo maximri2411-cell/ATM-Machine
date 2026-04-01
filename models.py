eaa16c12c946a645cdb059933f17b3cbba85c659
Maxim <maximri2411@gmail.com>
2026-04-01 10:58:22 +0300

    Again

--- models.py
+++ models.py
@@ -1,5 +1,28 @@
 import random 
 
+class Accounts:
+  
+    def __init__(self, account_id, name, PIN, Balance, is_active , History):
+        self.account_id = account_id
+        self.name = name
+        self.PIN = PIN
+        self.Balance = Balance
+        self.is_active = is_active
+        self.History = History
+
+def get_info(self):
+        print(f"ID:{self.account_id}, Dear {self.name}") 
+        if self.is_active == True:
+            print("Your account is Active")
+        else:
+            print("Your account is blocked")
+        if self.is_active == True:
+            print(f"Your PIN:{self.PIN} Your Balance:{self.Balance} NIS \n{self.History}")
+        else:
+            print("Can't continue the process, plaese call customer service")
+
+
+#============= Maxim ============
 # Creating a class of the Bank Part 3: it will manager all the accounts in the bank
 class Bank: 
     
@@ -101,25 +124,4 @@ class Bank:
 # print("\nlist all accounts")
 # my_bank.list_accounts()
 
-
-class Accounts:
-  
-    def __init__(self, account_id, name, PIN, Balance, is_active , History):
-        self.account_id = account_id
-        self.name = name
-        self.PIN = PIN
-        self.Balance = Balance
-        self.is_active = is_active
-        self.History = History
-
-def get_info(self):
-        print(f"ID:{self.account_id}, Dear {self.name}") 
-        if self.is_active == True:
-            print("Your account is Active")
-        else:
-            print("Your account is blocked")
-        if self.is_active == True:
-            print(f"Your PIN:{self.PIN} Your Balance:{self.Balance} NIS \n{self.History}")
-        else:
-            print("Can't continue the process, plaese call customer service")
 
