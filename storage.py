5a0a1ec54747ee873ae23974d38547e60369b48f
Maxim <maximri2411@gmail.com>
2026-04-02 12:31:39 +0300

    Added new function to test the translet from python to json

--- storage.py
+++ storage.py
@@ -6,14 +6,14 @@ def load_data():
     bank = Bank() # Creating an empty bank
     
     # TODO Here we put all of the json we have in one value
-    data = json.load()
+    data = json.load(open('data.json')) #! Important for the python to open the json info and data to translate for himself and work
     
     all_accounts = data["Accounts"] # Finding all of the accounts we created/have
 
-    for account_id in all_accounts[account_id]:
+    for account_id in all_accounts: # Going through the entire list of accounts
         info = all_accounts[account_id]
 
-        new_account = Accounts( #! Creating new account, do not change
+        new_account = Accounts( #! Creating new account, do not change, its base on what we created in data.json
             info['account_id'],
             info['full_name'],
             info['pin'],
@@ -25,6 +25,20 @@ def load_data():
         bank.Accounts[account_id] = new_account # Saving the new account in our bank
         
     return bank
+
+def save_data(bank): 
+    
+    data_to_save = {} # This is creating an empty dictionary to translet to json the python objects
+    
+    for account_id in bank.Accounts:
+        account_js = bank.Accounts[account_id] # Withdrawing accounts from current list
+        data_to_save[account_id] = account_js.j_dict() # Taking account and trsnslet from python to json
+
+    last_json = {"Accounts": data_to_save} #! All the accounts will be under the Accounts title, pay attention
+    
+    json_dump(last_json, open('data.json', 'w'), indent=4) # Taking the dictionary into the file that has been created 
+    # The w is for write, over write the file to new one, like > in linux
+    # indent is for beuty
 
 
 
