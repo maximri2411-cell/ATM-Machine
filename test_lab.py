f928e06095fa4b6ff25993951c2c669e71351451
Maxim <maximri2411@gmail.com>
2026-04-02 17:03:47 +0300

    Moving forward with my test

--- test_lab.py
+++ test_lab.py
@@ -4,24 +4,31 @@ from models import Accounts, Bank
 #! do not change somthing without asking him first
 
 account_for_test = Bank.find_account("321321") # Taking grisha for example
+if account_for_test:
+    print(f"current balance: {account_for_test.balance}")
+
+    print("test deposit")
+account_for_test.deposit(507.65)
+print(f"balance after the deposit: {account_for_test.balace}")
 
 
 
-#! This is for maxim Checkes for the json and python 
 
-from storage import load_data
+# #! This is for maxim Checkes for the json and python 
+# # Porpose of the test: we take the load.data function from storage file, turning it alive into python 
 
-print("Test")
-test_bank = load_data()
+# from storage import load_data
 
-grisha_id = "321321"
-if grisha_id in test_bank.Accounts:
-    acc_test = test_bank.Accounts[grisha_id]
-    print(f"found {acc_test.full_name}")
-    print(f"his balance: {acc_test.balance}")
+# print("Test")
+# test_bank = load_data()
+
+# grisha_id = "321321"
+# if grisha_id in test_bank.Accounts:
+#     acc_test = test_bank.Accounts[grisha_id]
+#     print(f"found {acc_test.full_name}")
+#     print(f"his balance: {acc_test.balance}")
     
-else:
-    print(f"Error: account was not found {grisha_id}.")
+# else:
+#     print(f"Error: account was not found {grisha_id}.")
     
     
-# Porpose of the test: we take the load.data function from storage file, turning it alive into python 
