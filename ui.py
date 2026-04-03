52c910317743d57972485f25fa9af528c771a9b1
Maxim <maximri2411@gmail.com>
2026-04-03 18:37:42 +0300

    Started with somthing, need to figure out why it dosent work

--- ui.py
+++ ui.py
@@ -2,7 +2,23 @@
 import tkinter as tk
 from tkinter import messagebox
 from storage import load_data, save_data
+import os
+from sys import platform
+import customtkinter
 
-        
+
+width = 700 
+height = 400
+
+root = customtkinter.CTk()
+root.title("Login System")
+
+x = (root.winfo_screenwidth() // 2) - (width // 2)
+y = (root.winfo_screenheight() // 3) - (width // 3)
+root.geometry("{}x{}+{}+{}".format(width, height, x, y))
+root.resizable(False, False)
 
 
+
+
+root.mainloop() # Activate the app 
