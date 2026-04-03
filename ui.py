7bef2f756696324aaa4a65640eaba5abe37081e9
Maxim <maximri2411@gmail.com>
2026-04-03 20:14:54 +0300

    Trying to add some function ive seen on some web to make it better

--- ui.py
+++ ui.py
@@ -8,15 +8,22 @@ class ATM_app: # Creating the class for the app
     def __init__(self, root):
         self.root = root
         self.root.title("ATM Machine") # The title
-        self.root.geometry("400x500") # Size of ther title
-#====================================
-        
+        self.root.geometry("800x600") # Size of ther title
         self.bank = load_data # Here we use all the function we created in the other files
+        self.current_user = None 
+        self.create_login_screen()
+        
+    def cleaning_screen(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        
+    def create_login_xcreen(self):
+        self.cleaning_screen()
         
         self.current_user = None # Here we save the Accounts after the log
         
         # Simple window just to see if its work with some feature like the type of language
-        tk.Label(self.root, text="Bank", font=("Ariel", 18)).pack(pady=20)
+        tk.Label(self.root, text="Welcome To X Bank", font=("Ariel", 23)).pack(pady=200)
         
         # Creating a button
         tk.Button(self.root, text="Check", command=self.test_data).pack()
