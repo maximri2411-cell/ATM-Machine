f33582cc555d9ac910eab015eba35b001de4da03
Maxim <maximri2411@gmail.com>
2026-04-03 19:46:47 +0300

    Never mind starting fresh

--- ui.py
+++ ui.py
@@ -1,27 +1,6 @@
-#This file made for all of our tkinter info/code 
-import tkinter as tk
-from tkinter import messagebox
-from storage import load_data, save_data
-import os
-from PIL import Image, ImageTk
-from sys import platform
-import customtkinter
+#====================================
+# All of features we need to make our app great
 
-def get
+#====================================
 
-# Creating our opening window
-width = 700 
-height = 400
 
-root = customtkinter.CTk()
-root.title("Login System")
-
-# Opening the window with the size we defined
-x = (root.winfo_screenwidth() // 2) - (width // 2)
-y = (root.winfo_screenheight() // 3) - (width // 3)
-root.geometry("{}x{}+{}+{}".format(width, height, x, y))
-root.resizable(False, False)
-
-# root.iconphoto("./Login-Sytem/key-person.png")
-
-root.mainloop() # Activate the app 
