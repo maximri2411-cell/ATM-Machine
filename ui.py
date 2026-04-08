172c21af147d2e1c6f962dfc2759f4670027401f
Alex.dv <alexdv@gmail.com>
2026-04-08 23:38:46 +0300

    change name

--- ui.py
+++ ui.py
@@ -34,7 +34,7 @@ class ATM_app: # Creating the class for the app
         self.cleaning_screen()
         
         # Simple window just to see if its work with some feature like the type of language
-        tk.Label(self.root, text="GTA ATM", font=("Arial", 36, "bold"), bg="#0a192f", fg="ivory").pack(pady=50)
+        tk.Label(self.root, text="GRISHA'S ATM", font=("Arial", 36, "bold"), bg="#0a192f", fg="ivory").pack(pady=50)
         
         # Adding fild to enter his ID number
         tk.Label(self.root, text="Account ID",font=("Arial", 16, "bold"), bg="#0a192f", fg="ivory").pack()
